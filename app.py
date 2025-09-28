8eb4b923925c0cb5e833494235313c7e91ce77cf
Mészáros József <meszarosjozsef20020403@mailbox.unideb.hu>
2025-09-28 21:27:32 +0200

    build: add functions in javascript to web application and add server module for running web app

---
+++ app.py
@@ -0,0 +1,38 @@
+import os, secrets, re
+
+from flask import (Flask,
+                   render_template,
+                   request,
+                   redirect,
+                   url_for,
+                   jsonify)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+
+@app.route('/')
+def home():
+    return render_template('welcome.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.get('level')
+    return redirect(url_for('level_page', level=data))
+
+@app.route('/level/<int:level>')
+def level_page(level: int):
+    try:
+        if level in range(1,4):
+            return render_template('quiz.html', level=level)
+        else:
+            raise ValueError("Invalid value...")
+    except:
+        return render_template(), 400
+
+if __name__ == "__main__":
+    app.run(
+        host = '0.0.0.0',
+        port = 8080,
+        threaded = True,
+        debug = True
+    )
